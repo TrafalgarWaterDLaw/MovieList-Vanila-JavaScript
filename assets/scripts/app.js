6c5366c2496101aa1269c0ddfaaeadb6d3bcfd54
Richard Gwati <haggio0844@gmail.com>
2021-07-03 16:38:13 +0200

    JS functionality fully operational

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,29 @@
-const addMovieModal = document.getElementById('add-modal');
-const startAddMovieButton = document.querySelector('header button');
+//Constants and Variables
+const addMovieModal = document.getElementById('add-modal')
+const startAddMovieButton = document.querySelector('header button')
 const backdrop = document.getElementById('backdrop')
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive')
 
 //Functions
-const toggleMovieModal = () => { 
-  addMovieModal.classList.toggle('visible');
-};
+
+const toggleMovieBackdrop = () => {
+	backdrop.classList.toggle('visible')
+}
+
+const cancelAddMovie = () => {
+	toggleMovieModal()
+}
+
+const toggleMovieModal = () => {
+  addMovieModal.classList.toggle('visible')
+  toggleMovieBackdrop()
+}
+
+const backdropClickHandler = () => {
+	toggleMovieModal()
+}
 
 //Events
-startAddMovieButton.addEventListener('click', toggleMovieModal);
+startAddMovieButton.addEventListener('click', toggleMovieModal)
+backdrop.addEventListener('click', toggleMovieModal)
+cancelAddMovieButton.addEventListener('click', cancelAddMovie)
